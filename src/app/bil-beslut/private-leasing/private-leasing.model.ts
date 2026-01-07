83aa7059d3048918e9d1d67263a8ca142e94c8cd
Fredrik Mile <fredrik.mile@gmail.com>
2026-01-07 21:35:44 +0100

    estimates

--- src/app/bil-beslut/private-leasing/private-leasing.model.ts
+++ src/app/bil-beslut/private-leasing/private-leasing.model.ts
@@ -12,6 +12,7 @@ export class PrivateLeasing {
         readonly serviceCost: number,
         readonly insuranceCost: number,
         readonly winterTiresCost: number,
+        readonly taxCost: number,
         readonly sourceUrl: string,
         readonly imageUrl: string | null,
         readonly estimatedFuelOrElectricity: number,
@@ -26,20 +27,105 @@ export class PrivateLeasing {
             FuelType[dto.fuel_type as keyof typeof FuelType],
             dto.standard_period,
             dto.standard_milage,
-            Math.floor(Math.random() * 400) + 200,
-            Math.floor(Math.random() * 500) + 300,
+            0,
+            this.estimateInsuranceCost(dto),
             dto.winter_tires_cost,
+            this.estimateTaxCostPerMonth(dto),
             dto.source_url,
             dto.image_url,
-            Math.floor(Math.random() * 800) + 200
+            this.estimateFuelOrElectricityCost(dto)
         )
     };
+
+    private static estimateFuelOrElectricityCost(dto: PrivateLeasingDTO): number {
+        const kmPerMonth = (dto.standard_milage * 10) / 12;
+
+        const ft = FuelType[dto.fuel_type as keyof typeof FuelType]
+
+        switch (ft) {
+            case FuelType.Electric:
+                return Math.round((18 / 100) * kmPerMonth * 2.5);
+
+            case FuelType.Petrol:
+                return Math.round((6.5 / 100) * kmPerMonth * 19.5);
+
+            case FuelType.Diesel:
+                return Math.round((5.5 / 100) * kmPerMonth * 21.0);
+
+            case FuelType.Hybrid:
+                return Math.round((4.0 / 100) * kmPerMonth * 19.5);
+
+            default:
+                return 0;
+        }
+    }
+
+
+    private static estimateInsuranceCost(
+        dto: PrivateLeasingDTO
+    ): number {
+
+        let base = 450;
+
+        switch (FuelType[dto.fuel_type as keyof typeof FuelType]) {
+            case FuelType.Electric:
+                base = 350;
+                break;
+            case FuelType.Hybrid:
+                base = 400;
+                break;
+            case FuelType.Diesel:
+                base = 500;
+                break;
+        }
+
+        const totalCarPrice = dto.base_cost * dto.standard_period;
+        const priceFactor =
+            Math.max(0, totalCarPrice - 300_000) / 100_000;
+
+        return Math.round(base + priceFactor * 50);
+    }
+
+    private static estimateTaxCostPerMonth(
+        dto: PrivateLeasingDTO
+    ): number {
+
+        const ft = FuelType[dto.fuel_type as keyof typeof FuelType]
+
+        if (ft === FuelType.Electric) return 0;
+
+        let yearlyBase: number;
+
+        switch (ft) {
+            case FuelType.Hybrid:
+                yearlyBase = 600;
+                break;
+            case FuelType.Petrol:
+                yearlyBase = 4200;
+                break;
+            case FuelType.Diesel:
+                yearlyBase = 6000;
+                break;
+        }
+
+        const estimatedCarValue = dto.base_cost * 36;
+
+        if (estimatedCarValue > 450_000) {
+            yearlyBase += 1_200;
+        } else if (estimatedCarValue > 350_000) {
+            yearlyBase += 600;
+        }
+
+        return Math.round(yearlyBase / 12);
+    }
+
+
 
     public getTotalMonthlyCost(): number {
         return this.baseCost +
             this.insuranceCost +
             this.winterTiresCost +
-            this.getTaxCostPerMonth() +
+            this.taxCost +
             this.serviceCost +
             this.estimatedFuelOrElectricity;
     }
@@ -49,22 +135,6 @@ export class PrivateLeasing {
         return (this.getTotalMonthlyCost() * this.standardPeriod);
     }
 
-    public getFullTaxCostPerYearInSek(): number {
-        switch (this.fuelType) {
-            case FuelType.Electric:
-                return 360;
-            case FuelType.Hybrid:
-                return 1080;
-            case FuelType.Diesel:
-                return 2160;
-            default:
-                return 1560;
-        }
-    }
-
-    public getTaxCostPerMonth(): number {
-        return this.getFullTaxCostPerYearInSek() / 12;
-    }
 }
 
 
